4b92a482de3e19d0a791f78964e977ade3089e9c
Firas Chikhaoui <47142784+firas-chikhaoui@users.noreply.github.com>
2023-06-20 12:22:48 +0200

    document App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,49 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import ProjectList from './components/ProjectList';
 
+// Define the shape of a Project object
 interface Project {
   id: string;
   name: string;
 }
 
 const App: React.FC = () => {
-  const [token, setToken] = useState<string | null>(null);
-  const [projects, setProjects] = useState<Project[]>([]);
+  // Declare state variables using the useState hook
+  const [token, setToken] = useState<string | null>(null); // Store authentication token
+  const [projects, setProjects] = useState<Project[]>([]); // Store an array of projects
 
+  // Handle login action
   const handleLogin = (token: string) => {
-    setToken(token);
-    fetchProjects(token);
+    setToken(token); // Update the token state variable
+    fetchProjects(token); // Fetch projects using the token
   };
 
+  // Handle logout action
   const handleLogout = () => {
-    setToken(null);
-    setProjects([]);
+    setToken(null); // Reset the token state variable
+    setProjects([]); // Clear the projects state variable
   };
 
+  // Fetch projects from the API using the provided token
   const fetchProjects = (token: string) => {
     // Make API call to fetch projects using the token
-    // Replace the placeholder URL with your actual API endpoint
     fetch('http://dev.june.local:8008/api/v2/project', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     })
-      .then((response) => response.json())
-      .then((data) => setProjects(data))
-      .catch((error) => console.log(error));
+      .then((response) => response.json()) // Parse the response as JSON
+      .then((data) => setProjects(data)) // Update the projects state variable with the fetched data
+      .catch((error) => console.log(error)); // Log any errors that occur
   };
 
   return (
     <div className="App">
       {!token ? (
+        // Render the LoginForm component if the user is not logged in
         <LoginForm onLogin={handleLogin} />
       ) : (
+        // Render the project list and logout button if the user is logged in
         <>
           <h2 className="main-heading">Project List</h2>
           <ProjectList projects={projects} onLogout={handleLogout} />
