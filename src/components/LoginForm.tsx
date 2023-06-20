4a704928edc0a941663ba867c390c0e116713ae6
Firas Chikhaoui <47142784+firas-chikhaoui@users.noreply.github.com>
2023-06-20 12:23:53 +0200

    document LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 
+// Define the type for the props received by the LoginForm component
 interface LoginFormProps {
-  onLogin: (username: string, password: string) => void;
+  onLogin: (username: string, password: string) => void; // Callback function triggered on form submission
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // Declare state variables using the useState hook
+  const [username, setUsername] = useState(''); // Store the username
+  const [password, setPassword] = useState(''); // Store the password
 
+  // Event handler for username input change
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+    setUsername(event.target.value); // Update the username state variable
   };
 
+  // Event handler for password input change
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
+    setPassword(event.target.value); // Update the password state variable
   };
 
+  // Event handler for form submission
   const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    onLogin(username, password);
+    event.preventDefault(); // Prevent the default form submission behavior
+    onLogin(username, password); // Call the onLogin function provided as a prop, passing the username and password as arguments
   };
 
   return (
